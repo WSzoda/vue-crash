554cc629213821bf2b6d3368099e3f157eba302e
Wojciech Szoda <wojciechszoda@icloud.com>
2023-03-10 09:00:48 +0100

    Displaying info baseing on id in route

---
+++ routerLearning/src/components/Nav.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <div>
+        <RouterLink to='/' active-class="active">Home</RouterLink>
+        <RouterLink to="/about" active-class="active">About</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+    .active {
+        font-weight: 900;
+        color: rgb(175, 116, 6)
+    }
+</style>
---
+++ routerLearning/src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<script setup>
+    import cars from "../data/cars.json"
+    import { RouterLink } from "vue-router"
+</script>
+
+<template>
+    <div>
+        <h1>Home</h1>
+        <div class="cars">
+            <RouterLink :to="`/car/${car.id}`" v-for="car in cars" :key="car.id">
+                {{ car.name }}
+            </RouterLink>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .cars {
+        display: flex;
+    }
+
+    .cars a {
+        text-decoration: none;
+        color: black;
+        width: 100%;
+        margin: 10px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.541);
+    }
+</style>
