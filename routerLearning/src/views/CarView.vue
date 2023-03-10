a57e18ba9f016e807db107cb15bbb7c38feb0bc6
Wojciech Szoda <wojciechszoda@icloud.com>
2023-03-10 09:19:01 +0100

    rendering children views inside parent view

--- routerLearning/src/views/CarView.vue
+++ routerLearning/src/views/CarView.vue
@@ -1,10 +1,13 @@
 <script setup>
-    import { useRoute } from 'vue-router';
+    import { useRoute, useRouter, RouterView } from 'vue-router';
     import cars from "../data/cars.json"
 
-    const route = useRoute()
 
-    const car = cars.find(c => c.id === parseInt(route.params.id))
+    const route = useRoute()
+    const router = useRouter()
+
+    const carId = parseInt(route.params.id)
+    const car = cars.find(c => c.id === carId)
     console.log(car)
 </script>
 
@@ -14,5 +17,7 @@
         <p>{{ car.name }}</p>
         <p>{{ car.year }}</p>
         <p>{{ car.price }}</p>
+        <button @click="router.push(`/car/${carId}/contact`)">Click to show contact info</button>
+        <RouterView/>
     </div>
 </template>
